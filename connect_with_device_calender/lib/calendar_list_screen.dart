c0cec317d437faa8922a8a8e52b84f967f8c0090
eslam <eslammongy995@gmail.com>
2022-01-20 12:20:29 +0200

    setup app structure

---
+++ connect_with_device_calender/lib/calendar_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalendarListScreen extends StatelessWidget {
+  const CalendarListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
