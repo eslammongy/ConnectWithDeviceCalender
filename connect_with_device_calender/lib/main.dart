0108b7a09f80f6b2f8459eed2a114df0b9291abf
eslam <eslammongy995@gmail.com>
2022-01-20 12:49:01 +0200

    get device calendar

--- connect_with_device_calender/lib/main.dart
+++ connect_with_device_calender/lib/main.dart
@@ -40,7 +40,7 @@ class CheckDeviceCalendars extends StatelessWidget {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const CalendarListScreen()));
+                        builder: (context) => CalendarListScreen()));
               }
             });
           },
